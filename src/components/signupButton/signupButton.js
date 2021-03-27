04df0b15a8327aefaed7ab722e91b164e76a7684
danihr23 <73990993+danihr23@users.noreply.github.com>
2021-03-27 19:46:01 +0200

    HomePage_added

---
+++ src/components/signupButton/signupButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./signupButton.css";
+export default function signupButton() {
+    return (
+        <div className="signupButton">
+            <p>Sign Up</p>
+        </div>
+    )
+}
---
+++ src/pages/home/home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import SignupButton from '../../components/signupButton/signupButton'
+
+import"./home.css";
+
+export default function Home() {
+    return (
+        <div className="home" style={{ 
+            backgroundImage: `url("background.jpg")`}}>
+                <img className="logo-text" src="logo-text.png" alt="Logo"/> 
+            <div className="home-nav">
+
+            <img src="logo-image.png" alt="Logo"/> 
+
+            <SignupButton/>
+
+            <p className="home-text"> Already have an account? Log in! </p>
+            </div> 
+            
+          
+            
+        </div>
+    )
+}
