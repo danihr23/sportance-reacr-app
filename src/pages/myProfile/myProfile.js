5e4735490b3ec3e1a7946a0e2796b88efbfd851e
danihr23 <73990993+danihr23@users.noreply.github.com>
2021-06-30 15:47:50 +0300

    update_myProfile

--- src/pages/myProfile/myProfile.js
+++ src/pages/myProfile/myProfile.js
@@ -3,11 +3,18 @@ import Nav from '../../containers/CreatePostNav/createPostNav'
 
 export default function MyProfile(params) {
 
+    
+
     let user = JSON.parse(localStorage.getItem('user'));
+    const userId = user.uid;
     console.log(user.uid)
+
+    let category =["basketball","tennis","football","Other","NFL"]
     return (
         <div>
             <Nav />
+
+            <h1> Welcome {user.email} </h1>
         </div>
 
 
