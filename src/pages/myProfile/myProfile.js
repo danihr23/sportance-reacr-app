57f86c061937f876f050fcff5c98c556fbd37e30
danihr23 <73990993+danihr23@users.noreply.github.com>
2021-06-30 14:25:41 +0300

    myProfile_stillInProgress

--- src/pages/myProfile/myProfile.js
+++ src/pages/myProfile/myProfile.js
@@ -3,9 +3,8 @@ import Nav from '../../containers/CreatePostNav/createPostNav'
 
 export default function MyProfile(params) {
 
-
-
-
+    let user = JSON.parse(localStorage.getItem('user'));
+    console.log(user.uid)
     return (
         <div>
             <Nav />
